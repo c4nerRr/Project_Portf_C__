2cff34888bc5d6f817dedf4bbaa4d6eef06bde68
C4nerr <vershu2007@gmail.com>
2026-06-02 21:07:41 +0300

    Initial commit

---
+++ LogParser.cpp
@@ -0,0 +1,23 @@
+//
+// Created by c4ner on 6/2/2026.
+//
+
+#include "LogParser.h"
+#include "DataTypes.h"
+#include <fstream>
+#include <mutex>
+#include <thread>
+#include <string_view>
+#include <unordered_map>
+#include <chrono>
+
+using namespace std;
+
+
+
+LogParser::LogParser() {
+}
+
+
+LogParser::~LogParser() {
+}
