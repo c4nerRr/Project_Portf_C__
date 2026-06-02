129dfd4939a2c95f808899bca236180c702da118
C4nerr <vershu2007@gmail.com>
2026-06-02 22:06:35 +0300

    LogParser.cpp _Upd1

--- LogParser.cpp
+++ LogParser.cpp
@@ -10,6 +10,7 @@
 #include <string_view>
 #include <unordered_map>
 #include <chrono>
+#include <iostream>
 
 using namespace std;
 
@@ -20,4 +21,35 @@ LogParser::LogParser() {
 
 
 LogParser::~LogParser() {
+    Clear();
+}
+
+void LogParser::from_main_to_main(const std::string &filePath) {
+        //std::thread t1(Reader);
+        Reader(filePath);
+
+        std::vector<thread> threads;
+        for (int i = 0; i<3; i++) {
+            threads.emplace_back (&LogParser::Consumer_log, this); //&ИмяКласса::ИмяМетода, + указатель на сам объект (this)
+        }
+        //Consumer_log();
+}
+
+void LogParser::printResults() {
+}
+
+
+void LogParser::Reader(const std::string &filePath) { //порциями данные из файла.
+    if (ifstream file(filePath); !file.is_open()) {
+        cerr << "Unable to open file " << filePath << endl;
+        exit(1);
+    }
+
+
+}
+
+void LogParser::Consumer_log() {
+}
+
+void LogParser::Clear() {
 }
