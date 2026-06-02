0f1aa77e79455fcbf5ed949d2b1edeb4884ec0e5
C4nerr <vershu2007@gmail.com>
2026-06-02 23:28:24 +0300

    LogParser.cpp _Upd2 Consumer_Log(v1) + Reader(v1) + from_main_to_main(v1)

--- LogParser.cpp
+++ LogParser.cpp
@@ -11,10 +11,7 @@
 #include <unordered_map>
 #include <chrono>
 #include <iostream>
-
 using namespace std;
-
-
 
 LogParser::LogParser() {
 }
@@ -25,30 +22,70 @@ LogParser::~LogParser() {
 }
 
 void LogParser::from_main_to_main(const std::string &filePath) {
-        //std::thread t1(Reader);
-        Reader(filePath);
-
         std::vector<thread> threads;
-        for (int i = 0; i<3; i++) {
+
+        threads.emplace_back(&LogParser::Reader, this, filePath);
+        cout << "DEBUG: start Reader threads" << endl;
+
+        for (int i = 0; i < 3; i++) {
             threads.emplace_back (&LogParser::Consumer_log, this); //&ИмяКласса::ИмяМетода, + указатель на сам объект (this)
+            cout << "DEBUG: start Consumer threads" << endl;
         }
-        //Consumer_log();
+
+        for (int i = 0; i < threads.size(); i++)
+        {
+            threads[i].join();
+        }
 }
 
 void LogParser::printResults() {
 }
 
 
-void LogParser::Reader(const std::string &filePath) { //порциями данные из файла.
-    if (ifstream file(filePath); !file.is_open()) {
+void LogParser::Reader(const std::string &filePath) {
+    ifstream file(filePath);
+    if (!file.is_open()) {
         cerr << "Unable to open file " << filePath << endl;
         exit(1);
     }
 
-
+    string line;
+    while (getline(file, line)) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        lines_.push_back(line);
+    }
+    isDone_ = true;
 }
 
 void LogParser::Consumer_log() {
+    do {
+        string currLine;
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (lines_.empty()) {
+                continue;
+            }
+            currLine = lines_.back();
+            lines_.pop_back();
+        }; //mutex
+
+        //.log struct: [дата время] УРОВЕНЬ: Сообщение
+        //example: [2026-06-02 20:45:01] ERROR: Database connection timeout
+
+        size_t startPos = currLine.find("] ") + 2;
+        if (startPos == std::string::npos) {
+            continue;
+        }
+        size_t endPos = currLine.find(":", startPos);
+        if (startPos == std::string::npos) { //NOLINT
+            continue;
+        }
+        string errorLevel = currLine.substr(startPos, endPos - startPos); //23 index + : on 28 index, 28-23 = word length
+        {
+            std::lock_guard<std::mutex> lock2(mutex_);
+            error_[errorLevel]++;
+        };
+    }while (!isDone_ || !lines_.empty());
 }
 
 void LogParser::Clear() {
